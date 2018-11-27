15202c5162f60f7c281f7ab5960bcef40da0252e
shagia <mrkraiola@gmail.com>
2018-11-27 14:42:25 -0800

    Clean up!

--- js/main.js
+++ js/main.js
@@ -1,24 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   // This is the bare minimum JavaScript. You can opt to pass no arguments to setup.
-  
   const a = document.querySelector('.item-video');
    // All videos must be loaded before initializing either the video player or the carousel.
-
-
   a.addEventListener('loadeddata', function() {
   console.log(a + " loaded!");
-
     const players = Plyr.setup('.item-video', {
       controls: [
         // 'play-large', // The large play button in the center
         'play', // The large play button in the center
-
       ],
       iconUrl: 'https://yr.media/statics/imgs/plyr.svg',
       fullscreen: { enabled: false },
       clickToPlay: false
     });
-
     var carousel = document.querySelector(".main-carousel")
     var flkty = new Flickity( carousel, {
       // options
@@ -27,10 +21,6 @@ document.addEventListener('DOMContentLoaded', () => {
       pageDots: true,
       draggable: true,
     });
-
   }, false);
-
-  
-
 });
 
