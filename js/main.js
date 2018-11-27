2d6ee6606c4141303c5ede160c612da8c1865787
shagia <mrkraiola@gmail.com>
2018-11-27 14:40:42 -0800

    video init fix

--- js/main.js
+++ js/main.js
@@ -1,23 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
   // This is the bare minimum JavaScript. You can opt to pass no arguments to setup.
-  const players = Plyr.setup('.item-video', {
-    controls: [
-      // 'play-large', // The large play button in the center
-      'play', // The large play button in the center
+  
+  const a = document.querySelector('.item-video');
+   // All videos must be loaded before initializing either the video player or the carousel.
 
-    ],
-    iconUrl: 'https://yr.media/statics/imgs/plyr.svg',
-    fullscreen: { enabled: false },
-    clickToPlay: false
-  });
 
-  var carousel = document.querySelector(".main-carousel")
-  var flkty = new Flickity( carousel, {
-    // options
-    cellAlign: 'center',
-    contain: true,
-    pageDots: true,
-    draggable: true,
-  });
+  a.addEventListener('loadeddata', function() {
+  console.log(a + " loaded!");
+
+    const players = Plyr.setup('.item-video', {
+      controls: [
+        // 'play-large', // The large play button in the center
+        'play', // The large play button in the center
+
+      ],
+      iconUrl: 'https://yr.media/statics/imgs/plyr.svg',
+      fullscreen: { enabled: false },
+      clickToPlay: false
+    });
+
+    var carousel = document.querySelector(".main-carousel")
+    var flkty = new Flickity( carousel, {
+      // options
+      cellAlign: 'center',
+      contain: true,
+      pageDots: true,
+      draggable: true,
+    });
+
+  }, false);
+
+  
 
 });
+
