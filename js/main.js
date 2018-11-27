568ab6c849a917c267ead5cfcb368ea2c31fa420
Radames Ajna <radamajna@gmail.com>
2018-11-27 13:15:08 -0800

    using plyr player

---
+++ js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // This is the bare minimum JavaScript. You can opt to pass no arguments to setup.
+  const players = Plyr.setup('.item-video', {
+    controls: [
+      'play-large', // The large play button in the center
+    ],
+    iconUrl: 'https://yr.media/statics/imgs/plyr.svg',
+    fullscreen: { enabled: false },
+    clickToPlay: false
+  });
+
+  var carousel = document.querySelector(".main-carousel")
+  var flkty = new Flickity( carousel, {
+    // options
+    cellAlign: 'center',
+    contain: true,
+    pageDots: true,
+    draggable: true,
+  });
+
+});
