6214fda0a002f725df872bc5c6b742544ee44ed0
Radames Ajna <radamajna@gmail.com>
2018-11-30 16:04:32 -0800

    swiper arrows, social icons

--- js/main.js
+++ js/main.js
@@ -25,6 +25,10 @@ function startVideoSlider() {
     pagination: {
       el: '.swiper-pagination',
     },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   });
 
 }
